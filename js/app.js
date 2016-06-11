fc6544354acc96062707d2d098f0afe800c56992
RayJustin <Ray.Justin@live.com>
2016-06-10 19:10:06 -0700

    Updated

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+// Add new item
+$("img.add").on("click", function() {
+	addItem();
+});
+
+// Cross off clicked item
+$('.list').on('click','li .fa-check',function() {
+	$(this).parent().toggleClass('checked');
+});
+
+// Delete checked Items
+$('img.delete').click(function() {
+	$('li.checked').remove();
+});
+
+// Use Enter key to add item
+$('.input').keydown(function(e){
+	if(e.which == 13){
+		addItem();
+}});
+
+});
+
+
+function addItem() {
+	$('ul.list').prepend('<li><i class="fa fa-check"> '+ $('.input').val() +' </li>')
+	$('.input').val('')
+}
+
+
+
+
+
